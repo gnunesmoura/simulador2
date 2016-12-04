92de1c220025cee84e4ee4883ac653a0fbdc0496
Gustavo Nunes <gustavo.moura@computacao.ufla.br>
2016-12-04 19:48:05 -0200

    Main. W

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <init/instance/node.hpp>
+#include <init/instance/instance.hpp>
+#include <init/dao/instance_read.hpp>
+using namespace std;
+
+int main (int argc, char* argv[]) {
+    if(argc != 2) {
+        cout << "Para usar o sistema passe como parametro o nome da instancia.\nEx: 4.1-1\n";
+        return -1;
+    }
+
+    sim::Instance_reader reader(argv[argc-1]);
+    auto instance = reader.read_instance ();
+
+
+    return 0;
+}
