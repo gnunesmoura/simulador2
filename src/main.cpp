0bd9fc2cbb6e4a648398a17914ea3558b50c667a
Gustavo Nunes <gustavo.moura@computacao.ufla.br>
2016-12-07 14:45:08 -0200

    w.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <stdio.h>
 
-#include <init/instance/node.hpp>
+
 #include <init/instance/instance.hpp>
-#include <init/dao/instance_read.hpp>
-#include <init/motion/movement.hpp>
+#include <init/dao/instance_da.hpp>
+#include <init/system/system.hpp>
+#include <init/evaluation/evaluation.hpp>
 
 using namespace std;
+
 
 int main (int argc, char* argv[]) {
     if(argc != 2) {
@@ -15,20 +18,20 @@ int main (int argc, char* argv[]) {
 
     sim::Instance_reader reader(argv[argc-1]);
     auto instance = reader.read_instance ();
+    auto real = reader.read_real_instance ();
 
-    auto nodes = instance->nodes ();
+    sim::System s(*(instance.get()));
 
-    sim::Movement m (nodes[0], 0, 0);
-    
-    cout << m.m_static << endl;
+    s.solve ();
 
-    while (m.move(false));
+    sim::Evaluation r (*(instance.get ()), *(real.get ()));
 
-    m.release_stress ();
-    
-    for (auto axiss: nodes[0]->pos()) cout << axiss << " ";
-    cout << endl;
+    r.show_erro(true, 0.01);
 
-    
+    printf("[desvMed\t%.10lf\tdesvMax\t%.10lf\tdesvQuad\t%.10lf\trmsd\t%.10lf\tpar\t%.10lf ]\n", r.m_desv_med, r.m_desv_max, r.m_desv_quad, r.m_rmsd, r.m_par);
+
+    reader.print_result_file (*(instance.get ()));
+
     return 0;
 }
+    
