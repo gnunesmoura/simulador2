dbe88a2fcca22faec898b6fa25d522c27d8eab5d
Gustavo Nunes <gustavo.moura@computacao.ufla.br>
2016-12-08 00:03:47 -0200

    Created function dist.

--- include/init/instance/edge.hpp
+++ include/init/instance/edge.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <valarray>
 #include <utility>
 
 namespace sim {
@@ -10,5 +11,14 @@ class Node;
 using edge = std::pair<double, Node&>;
 
 using edges = std::vector<edge>;
+
+const double dist (const Node& arg0, const Node& arg1) {
+    std::valarray<double> vec (arq1.pos () - arg0.pos ());
+    vec = std::pow(vec, 2.0);
+    double sqr_sum = vec.sum ();
+
+    if(sqr_sum > 0) std::sqrt(sqr_sum);
+    return 0;
+}
 
 } //namespace
