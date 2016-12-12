30955d2e39601e5dfd665386d4ba2f4e9ca53369
Gustavo Nunes <gustavo.moura@computacao.ufla.br>
2016-12-12 16:47:57 -0200

    main now calls the python script "exibe_redes_comparacao.py" by system call and show the diference between the rede file and result file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <string>
 
 #include <init/instance/instance.hpp>
 #include <init/dao/instance_da.hpp>
@@ -11,12 +11,12 @@ using namespace std;
 
 
 int main (int argc, char* argv[]) {
-    if(argc != 2) {
+    if(argc < 2) {
         cout << "Para usar o sistema passe como parametro o nome da instancia.\nEx: 4.1-1\n";
         return -1;
     }
     
-    sim::Instance_reader reader(argv[argc-1]);
+    sim::Instance_reader reader(argv[1]);
     auto instance = reader.read_instance ();
     auto real = reader.read_real_instance ();
 
@@ -25,16 +25,21 @@ int main (int argc, char* argv[]) {
     s.solve ();
 
     sim::Evaluation r (*(instance.get ()), *(real.get ()));
-
     auto nodes = instance->nodes ();
-    for (auto node: nodes) node->print_geo ();
 
-    r.show_erro(false, 0.01);
+    r.show_erro(true, 0.01);
 
     cout << '\n';
-    cout << "[desvMed " << r.m_desv_med << " desvMax " << r.m_desv_max << " desvQuad " << r.m_desv_quad << " rmsd " << r.m_rmsd << " par " << r.m_par << "]\n\n";
+    cout << "[desvMed " << r.m_desv_med << " desvMax " << r.m_desv_max << " desvQuad " << r.m_desv_quad << " rmsd " << r.m_rmsd << " par " << r.m_par << "]\n";
 
-    reader.print_result_file (*instance);
+    // reader.print_result_file (*instance);
+
+    cout << instance->radio_range () << " " << instance->noise () << '\n';
+    if (argc > 2) {
+        string show("python3 instancias/exibe_redes_comparacao.py ");
+        show +=  reader.m_result_file + " " + reader.m_rede_file;
+        system(show.c_str ());
+    }
     return 0;
 }
     
