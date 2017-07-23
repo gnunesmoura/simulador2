6a9b07f9eed2269bc3c1ef05c4d3e778126a72af
Gustavo Nunes <gnunes.moura@gmail.com>
2017-07-23 19:58:54 -0300

    Ordem de precedencia dos nós implementada e testada

--- src/init/system/system.cpp
+++ src/init/system/system.cpp
@@ -74,28 +74,45 @@ void System::solve_tree () {
     });
 
     int i = 0;
-    while (i != queue.size()) {
+    while (i!= queue.size()) {
 
-        find_place(queue[i]);
-        auto neighbors = queue[i]->neighbors();
-        std::for_each (neighbors.begin (), neighbors.end (), [&](edge t_edge){
-            if(t_edge.second.type() == not_placed && t_edge.second.placeds_size() + t_edge.second.anchors_size() >= 3){
-                t_edge.second.new_type(queued);
-                queue.push_back(&(t_edge.second));
+        while (i != queue.size()) {
+
+            find_place(queue[i]);
+            auto neighbors = queue[i]->neighbors();
+            std::for_each (neighbors.begin (), neighbors.end (), [&](edge t_edge){
+                if(t_edge.second.type() == not_placed && t_edge.second.placeds_size() + t_edge.second.anchors_size() >= 3){
+                    t_edge.second.new_type(queued);
+                    queue.push_back(&(t_edge.second));
+                }
+            }); 
+            for (auto& move: m_moves) {
+                move_until_stop (move, true);
             }
-        }); 
-        for (auto& move: m_moves) {
-            move_until_stop (move, true);
+            i++;
         }
-        i++;
-    }
 
-    for (auto n: nodes) {
-        if(n->type() != placed && n->placeds_size() + n->anchors_size() >= 2) {
-            find_place(n);
+        auto node = std::find_if(nodes.begin(), nodes.end(), [](Node* n) {
+            return n->type() != placed && n->placeds_size() + n->anchors_size() == 2;
+        });
+
+        if(node != nodes.end()) {
+            Node* chosen = *node;
+            for (auto n: nodes) {
+                if (n->type() != placed && n->placeds_size() + n->anchors_size() >= 2) {
+                    if (n->anchors_size() > chosen->anchors_size()) {
+                        chosen = n;
+                    } else if (n->placeds_size() + n->anchors_size() > chosen->placeds_size() + chosen->anchors_size()) {
+                        chosen = n;
+                    }
+                }
+            }
+
+            queue.push_back(chosen);
         }
-    }
 
+    }
+    
     for (auto n: nodes) {
         if(n->type() != placed) {
             find_place(n);
